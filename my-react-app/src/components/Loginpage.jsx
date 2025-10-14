7fe5a6713d39bf33debfd597ff5ddb8fd5adbbdd
Yash Kanwar <kanwarzen642@gmail.com>
2025-10-14 12:17:24 +0530

    login

--- my-react-app/src/components/Loginpage.jsx
+++ my-react-app/src/components/Loginpage.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-import './Loginpage.css'
 const LoginForm=()=>{
     return (
         <div className="Wrapped">
-            <form action="">
-                <h1>Login</h1>
+            <form action=''>
                 <div className="Input-box">
                     <input type="text" placeholder="Username"  /><br /><br />
                     <input type="password" placeholder="Password"  />
                 </div>
+
 
                 <div className="Register"><p>Dont have an account?  
                      <a href="#">Register</a> </p>
