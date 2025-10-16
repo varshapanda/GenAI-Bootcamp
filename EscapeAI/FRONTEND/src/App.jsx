178a6f88afade24c6e95bf7ad978a8dbf91ea6bc
Yash Kanwar <kanwarzen642@gmail.com>
2025-10-16 13:13:34 +0530

    Added RoomSelection Page

--- EscapeAI/FRONTEND/src/App.jsx
+++ EscapeAI/FRONTEND/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./components/LandingPage";
 import GoogleAuth from "./components/authentication/Auth";
+import RoomSelection from "./components/RoomSelection";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/auth" element={<GoogleAuth />} />
+        <Route path="/RoomSelect" element={<RoomSelection/>}/>
       </Routes>
     </BrowserRouter>
   );
